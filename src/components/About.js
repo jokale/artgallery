f047ce9089b1753c426258b916155b10f7ded80a
jokale <kalemajoanna@gmail.com>
2020-11-10 12:14:38 +0000

    add new stylinf to footer

--- src/components/About.js
+++ src/components/About.js
@@ -15,20 +15,22 @@ class About extends React.Component {
                 <br></br>In sollicitudin est nec mattis rutrum. In luctus neque at ligula tincidunt, a interdum nunc mollis.  <br></br>
                 <br></br>Duis ultrices augue non tortor laoreet ultricies. Nunc ut posuere velit. Donec fermentum viverra nunc et pellentesque. 
                 <br></br>Pellentesque tincidunt leo ipsum.</p>
-                <p id="p-test">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis laoreet neque. <br></br>
-                <br></br>In sollicitudin est nec mattis rutrum. In luctus neque at ligula tincidunt, a interdum nunc mollis.  <br></br>
-                <br></br>Duis ultrices augue non tortor laoreet ultricies. Nunc ut posuere velit. Donec fermentum viverra nunc et pellentesque. 
-                <br></br>Pellentesque tincidunt leo ipsum.</p>
-                <p id="p-test">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis laoreet neque. <br></br>
-                <br></br>In sollicitudin est nec mattis rutrum. In luctus neque at ligula tincidunt, a interdum nunc mollis.  <br></br>
-                <br></br>Duis ultrices augue non tortor laoreet ultricies. Nunc ut posuere velit. Donec fermentum viverra nunc et pellentesque. 
-                <br></br>Pellentesque tincidunt leo ipsum.</p>
-                <p id="p-test">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis laoreet neque. <br></br>
-                <br></br>In sollicitudin est nec mattis rutrum. In luctus neque at ligula tincidunt, a interdum nunc mollis.  <br></br>
-                <br></br>Duis ultrices augue non tortor laoreet ultricies. Nunc ut posuere velit. Donec fermentum viverra nunc et pellentesque. 
-                <br></br>Pellentesque tincidunt leo ipsum.</p>
+                
+                <div id='footer'>
+                <h6>26 abc lane <br></br> london <br></br> Sw13 5dx <br></br>+44 020 1234 5678</h6>
 
-           <Footer/>
+               <h6> <a href="https://www.instagram.com/"  target="_blank" rel="noopener noreferrer">Instagram</a><br></br>
+                <a href=" https://twitter.com/"  target="_blank " rel="noopener noreferrer" >Twitter</a><br></br>
+                <a href="https://www.termsfeed.com/blog/sample-terms-and-conditions-template/"  target="_blank" rel="noopener noreferrer">Terms and Conditions </a><br></br>
+                <a href="https://www.termsfeed.com/blog/sample-privacy-policy-template/"  target="_blank" rel="noopener noreferrer">Privacy policy </a></h6>
+
+                <br></br>
+                <h6><a href="https://joannathedeveloper.netlify.app/" target="_blank" rel="noopener noreferrer">J.E.Kalema Web Applications</a> </h6>
+
+
+
+             
+            </div>
             </div>
         )
     }
--- src/components/Work.js
+++ src/components/Work.js
@@ -24,7 +24,22 @@ class Work extends React.Component {
 
                
             <a href='https://design-milk.com/minimalist-artwork-reflecting-on-the-human-condition/' target="_blank" rel="noopener noreferrer" id='a-about'> orgin of work</a>
-            <Footer/>
+
+            <div id='footer-work'>
+                <h6>26 abc lane <br></br> london <br></br> Sw13 5dx <br></br>+44 020 1234 5678</h6>
+
+               <h6> <a href="https://www.instagram.com/"  target="_blank" rel="noopener noreferrer">Instagram</a><br></br>
+                <a href=" https://twitter.com/"  target="_blank " rel="noopener noreferrer" >Twitter</a><br></br>
+                <a href="https://www.termsfeed.com/blog/sample-terms-and-conditions-template/"  target="_blank" rel="noopener noreferrer">Terms and Conditions </a><br></br>
+                <a href="https://www.termsfeed.com/blog/sample-privacy-policy-template/"  target="_blank" rel="noopener noreferrer">Privacy policy </a></h6>
+
+                <br></br>
+                <h6><a href="https://joannathedeveloper.netlify.app/" target="_blank" rel="noopener noreferrer">J.E.Kalema Web Applications</a> </h6>
+
+
+
+             
+            </div>
             </div>
         )
     }
